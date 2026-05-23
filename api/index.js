1c427fdc624c095e81e61de49f8a3daea431413d
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-23 11:01:49 -0300

    chore: update shareCode to 9GGZ8FZ0YGE

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=6Z8C993HB59&afp8=oa&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=9GGZ8FZ0YGE&afp8=oa&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
