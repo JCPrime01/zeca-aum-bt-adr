421625ba387ff2e762084f2c652119bddb236e72
JCPrime01 <vanzokiller@gmail.com>
2026-05-31 11:23:19 -0300

    chore: shareCode X6C6A8RDPGJ -> 2GBU4CCULJG (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=X6C6A8RDPGJ&afp8=oa&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=2GBU4CCULJG&afp8=oa&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
