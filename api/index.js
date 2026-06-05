f7b3a25514479fc13c06b26928438d65258870a5
JCPrime01 <vanzokiller@gmail.com>
2026-06-05 14:03:42 -0300

    chore: shareCode XSO69UBI4BW -> NVDSFVGNCDJ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=XSO69UBI4BW&afp8=oa&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=NVDSFVGNCDJ&afp8=oa&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
