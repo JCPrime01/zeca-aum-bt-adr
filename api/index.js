8a7e76a1b9fbe894d153744612b27fdb49b995b2
JCPrime01 <vanzokiller@gmail.com>
2026-06-12 10:07:46 -0300

    chore: shareCode QTCTQVBBBRX -> A040LCTY2KI (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=QTCTQVBBBRX&afp2=oa&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=A040LCTY2KI&afp2=oa&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
