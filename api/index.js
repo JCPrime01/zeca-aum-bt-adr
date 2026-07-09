c92b1b6768062a444764e9818ec5d7ea5cdd2762
JCPrime01 <vanzokiller@gmail.com>
2026-07-09 11:02:22 -0300

    chore: shareCode FX26P98EI4H -> TJ5E5PPYP3U (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=FX26P98EI4H&afp2=oa&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=TJ5E5PPYP3U&afp2=oa&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
