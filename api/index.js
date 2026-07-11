41bac06b071af1ce014e4b7275dfddecf921bdf1
JCPrime01 <vanzokiller@gmail.com>
2026-07-11 11:14:44 -0300

    chore: shareCode L3NIDD479W5 -> Y15Z0KIIZJP (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=L3NIDD479W5&afp2=oa&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=Y15Z0KIIZJP&afp2=oa&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
