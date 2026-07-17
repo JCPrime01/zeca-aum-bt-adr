db87d6b3b01527b091926923f44c5a6865b934d8
JCPrime01 <vanzokiller@gmail.com>
2026-07-17 10:35:16 -0300

    chore: shareCode 1HW1G07V391 -> EY50E613DOR (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=1HW1G07V391&afp2=oa&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=EY50E613DOR&afp2=oa&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
