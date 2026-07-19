b1b63bb2802c04413d6b42ff12d178ab797bf3e9
JCPrime01 <vanzokiller@gmail.com>
2026-07-19 10:17:44 -0300

    chore: shareCode AGK3NUL6B71 -> FVBSDYVX2J0 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=AGK3NUL6B71&afp2=oa&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=FVBSDYVX2J0&afp2=oa&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
