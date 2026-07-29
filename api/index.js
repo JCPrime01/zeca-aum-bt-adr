36881812129b890ecafe7432a6d92a5e7b703ab5
JCPrime01 <vanzokiller@gmail.com>
2026-07-29 09:59:03 -0300

    chore: shareCode 58GAWI8EI2L -> OZ868Y1JRAU (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=58GAWI8EI2L&afp2=oa&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=OZ868Y1JRAU&afp2=oa&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
